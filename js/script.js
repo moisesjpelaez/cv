a46f3472f6dddf661b2ac76b6333d160a2513efc
moisesjpelaez <moises.jpelaez@gmail.com>
2019-06-11 15:07:44 -0400

    Basic functionality implemented.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 $(function()
-{
-    
+{    
+    function showSection(sectionID)
+    {
+        $("section").hide();
+        $(sectionID).fadeIn();
+    }
+
+    showSection("#experience");
+
+    $(".nav-link").click(function(){
+        event.preventDefault();
+
+        if($(this).hasClass("active"))
+            return;
+
+        $(".active").removeClass("active");
+        showSection($(this).attr("href"));
+        $(this).addClass("active");
+    });
 });
